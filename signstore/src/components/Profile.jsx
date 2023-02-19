15611cc2c4802c3c1718553c530fdcb94835b2bf
ahlemfarhani123 <ahlemfarhani2@gmail.com>
2023-02-18 17:38:43 -0800

    testt

--- signstore/src/components/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-async function getusers(db) {
-    const user = collection(db, 'users');
-    const userSnapshot = await getDocs(user);
-    const userList = userSnapshot.docs.map(doc => doc.data());
-    return userList;
-  }
